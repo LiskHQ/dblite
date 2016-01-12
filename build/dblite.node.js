b7e6623c442bffe4adae1211f91a3de3ec52239e
Olivier Beddows <olivier@lisk.io>
2016-01-12 20:14:27 +0000

    Adding JSON string parser.

--- build/dblite.node.js
+++ build/dblite.node.js
@@ -795,6 +795,12 @@ function row2parsed(row) {
       out[fields[i]] = parsers[i](row[i], 'hex');
     } else if (parsers[i] === Array) {
       out[fields[i]] = row[i] ? row[i].split(",") : [];
+    } else if (parsers[i] === String) {
+      try {
+        out[fields[i]] = JSON.parse(JSON.stringify((row[i] || "")));
+      } catch (e) {
+        out[fields[i]] = row[i];
+      }
     } else {
       out[fields[i]] = parsers[i](row[i]);
     }
